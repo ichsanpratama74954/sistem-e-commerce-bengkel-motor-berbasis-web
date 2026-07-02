eec8faf9e64f272adcc21c66ef73f9f8530d568c
InTAnCAns <ayuintanmariani.240604031@student.hamzanwadi.ac.id>
2026-07-02 20:55:03 +0800

    m

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
-    
-    // Perbaikan: Semua tanda '::' diganti menjadi '.' agar Livewire bisa membaca foldernya
-    Route::livewire('/categories', 'pages.category.index')->name('category.index');
-    Route::livewire('/services', 'pages.service.index')->name('service.index');
-    Route::livewire('/motorcycles', 'pages.motorcycle.index')->name('motorcycle.index');
-    Route::livewire('/orders', 'pages.order.index')->name('order.index');
+    Route::view('dashboard', 'dashboard') ->name('dashboard');
+    Route::livewire('/categories', 'pages::category.index')->name('category.index');
+    Route::livewire('/services', 'pages::service.index')->name('service.index');
+    Route::livewire('/motorcycles', 'pages::motorcycle.index')->name('motorcycle.index');
+    Route::livewire('/orders', 'pages::order.index')->name('order.index');
 });
 
 require __DIR__.'/settings.php';

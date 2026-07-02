ee0a1b396d54a8c8d85d98bd7ab07f017277ff4e
InTAnCAns <ayuintanmariani.240604031@student.hamzanwadi.ac.id>
2026-07-02 21:02:16 +0800

    s

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::livewire('/categories', 'pages::category.index')->name('category.index');
     Route::livewire('/services', 'pages::service.index')->name('service.index');
     Route::livewire('/motorcycles', 'pages::motorcycle.index')->name('motorcycle.index');
-    Route::livewire('/orders', 'pages::order.index')->name('order.index');
+    Route::livewire('/orders', 'pages::orders.index')->name('order.index');
 });
 
 require __DIR__.'/settings.php';
